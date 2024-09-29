df9edfcebbea09ae385fdf7b438a0b5fbb31f785
uwasepauline7 <puwase@andrew.cmu.edu>
2024-09-29 18:17:08 +0200

    changing c list

--- clist.c
+++ clist.c
@@ -1,3 +1,11 @@
+/*
+ * clist.c
+ * 
+ * Linked list implementation for ISSE Assignment 5
+ *
+ * Author: <your name here>
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -16,27 +24,32 @@ struct _clist {
     int length;
 };
 
-// Function to create a new node
+// Create (malloc) a new _cl_node and populate it with the supplied values
 static struct _cl_node* _CL_new_node(CListElementType element, struct _cl_node *next) {
-    struct _cl_node* new = (struct _cl_node*) malloc(sizeof(struct _cl_node));
-    assert(new);
-    new->element = element;
-    new->next = next;
-    return new;
+    struct _cl_node* new_node = (struct _cl_node*) malloc(sizeof(struct _cl_node));
+    assert(new_node);
+
+    new_node->element = element;
+    new_node->next = next;
+
+    return new_node;
 }
 
-// Create a new circular linked list
+// Documented in .h file
 CList CL_new() {
     CList list = (CList) malloc(sizeof(struct _clist));
     assert(list);
+
     list->head = NULL;
     list->length = 0;
+
     return list;
 }
 
-// Free the entire list
+// Documented in .h file
 void CL_free(CList list) {
     assert(list);
+
     struct _cl_node *current = list->head;
     struct _cl_node *next_node;
 
@@ -45,194 +58,211 @@ void CL_free(CList list) {
         free(current);
         current = next_node;
     }
+
     free(list);
 }
 
-// Return the length of the list
+// Documented in .h file
 int CL_length(CList list) {
     assert(list);
 #ifdef DEBUG
     int len = 0;
-    struct _cl_node *node = list->head;
-    while (node != NULL) {
+    for (struct _cl_node *node = list->head; node != NULL; node = node->next) {
         len++;
-        node = node->next;
     }
-    assert(len == list->length);
-    printf("Expected length: %d, Actual length: %d\n", list->length, len);
+    assert(len == list->length); // Ensure length is accurate
 #endif // DEBUG
+
     return list->length;
 }
 
-// Print the elements in the list
+// Documented in .h file
 void CL_print(CList list) {
     assert(list);
+
     int num = 0;
-    struct _cl_node *node = list->head;
-    while (node != NULL) {
+    for (struct _cl_node *node = list->head; node != NULL; node = node->next) {
         printf("  [%d]: %s\n", num++, node->element);
-        node = node->next;
     }
 }
 
-// Push a new element onto the list
+// Documented in .h file
 void CL_push(CList list, CListElementType element) {
     assert(list);
     list->head = _CL_new_node(element, list->head);
     list->length++;
 }
 
-// Pop an element from the front of the list
+// Documented in .h file
 CListElementType CL_pop(CList list) {
     assert(list);
+
+    if (list->head == NULL) return INVALID_RETURN; // Check if empty
+
     struct _cl_node *popped_node = list->head;
-    if (popped_node == NULL)
-        return INVALID_RETURN;
     CListElementType ret = popped_node->element;
+
     list->head = popped_node->next;
     free(popped_node);
     list->length--;
+
     return ret;
 }
 
-// Append an element to the end of the list
+// Documented in .h file
 void CL_append(CList list, CListElementType element) {
     assert(list);
+
     struct _cl_node *new_node = _CL_new_node(element, NULL);
     if (list->head == NULL) {
-        list->head = new_node;
+        list->head = new_node; // List was empty
     } else {
         struct _cl_node *current = list->head;
         while (current->next != NULL) {
-            current = current->next;
+            current = current->next; // Traverse to the end
         }
-        current->next = new_node;
+        current->next = new_node; // Append at the end
     }
     list->length++;
 }
 
-// Get the nth element from the list
+// Documented in .h file
 CListElementType CL_nth(CList list, int pos) {
     assert(list);
-    if (pos < 0 || pos >= list->length) {
-        return INVALID_RETURN;
-    }
+    if (pos < 0 || pos >= list->length) return INVALID_RETURN; // Invalid position
+
     struct _cl_node *current = list->head;
     for (int i = 0; i < pos; i++) {
-        current = current->next;
+        current = current->next; // Traverse to the position
     }
     return current->element;
 }
 
-// Insert an element at a specific position in the list
+// Documented in .h file
 bool CL_insert(CList list, CListElementType element, int pos) {
     assert(list);
-    if (pos < 0 || pos > list->length) {
-        return false;
-    }
+    if (pos < 0 || pos > list->length) return false; // Invalid position
+
     if (pos == 0) {
-        CL_push(list, element);
-        return true;
+        CL_push(list, element); // Insert at head
+    } else {
+        struct _cl_node *current = list->head;
+        for (int i = 0; i < pos - 1; i++) {
+            current = current->next; // Traverse to the insertion point
+        }
+        current->next = _CL_new_node(element, current->next); // Insert node
     }
-    struct _cl_node *new_node = _CL_new_node(element, NULL);
-    struct _cl_node *current = list->head;
-    for (int i = 0; i < pos - 1; i++) {
-        current = current->next;
-    }
-    new_node->next = current->next;
-    current->next = new_node;
+
     list->length++;
     return true;
 }
 
-// Remove an element at a specific position from the list
+// Documented in .h file
 CListElementType CL_remove(CList list, int pos) {
     assert(list);
-    if (pos < 0 || pos >= list->length) {
-        return INVALID_RETURN;
-    }
+    if (pos < 0 || pos >= list->length) return INVALID_RETURN; // Invalid position
+
     struct _cl_node *current = list->head;
+    CListElementType ret;
+
     if (pos == 0) {
-        return CL_pop(list);
+        return CL_pop(list); // Use pop to remove the head
+    } else {
+        for (int i = 0; i < pos - 1; i++) {
+            current = current->next; // Traverse to the node before the one to remove
+        }
+        struct _cl_node *to_remove = current->next; // Node to be removed
+        ret = to_remove->element;
+        current->next = to_remove->next; // Bypass the node
+        free(to_remove);
     }
-    for (int i = 0; i < pos - 1; i++) {
-        current = current->next;
-    }
-    struct _cl_node *removed_node = current->next;
-    if (removed_node == NULL) {
-        return INVALID_RETURN;
-    }
-    current->next = removed_node->next;
-    CListElementType ret = removed_node->element;
-    free(removed_node);
+
     list->length--;
     return ret;
 }
 
-// Copy an existing list
+// Documented in .h file
 CList CL_copy(CList src_list) {
     assert(src_list);
+  
     CList new_list = CL_new();
-    struct _cl_node *current = src_list->head;
-    while (current != NULL) {
-        CL_append(new_list, current->element);
-        current = current->next;
+    for (struct _cl_node *current = src_list->head; current != NULL; current = current->next) {
+        CL_append(new_list, current->element); // Append elements to new list
     }
+  
     return new_list;
 }
 
-// Insert an element in sorted order
+// Documented in .h file
 int CL_insert_sorted(CList list, CListElementType element) {
     assert(list);
-    struct _cl_node *new_node = _CL_new_node(element, NULL);
-    if (list->head == NULL || strcmp(list->head->element, element) >= 0) {
-        new_node->next = list->head;
-        list->head = new_node;
-        list->length++;
-        return 0; // inserted at the head
+  
+    if (list->head == NULL || strcmp(element, list->head->element) < 0) {
+        CL_push(list, element); // Insert at head if list is empty or element is smaller
+        return 1; // Inserted at the head
     }
+  
     struct _cl_node *current = list->head;
     int pos = 1;
+
     while (current->next != NULL && strcmp(current->next->element, element) < 0) {
-        current = current->next;
+        current = current->next; // Traverse to find the correct position
         pos++;
     }
-    new_node->next = current->next;
-    current->next = new_node;
+
+    current->next = _CL_new_node(element, current->next); // Insert node
     list->length++;
-    return pos; // inserted at position
+    return pos; // Return the position where it was inserted
 }
 
-// Join two lists together
+// Documented in .h file
 void CL_join(CList list1, CList list2) {
     assert(list1);
     assert(list2);
-    if (list2->head == NULL) return; // Nothing to join
+
+    if (list2->head == NULL) return; // If list2 is empty, do nothing
+
     if (list1->head == NULL) {
-        list1->head = list2->head;
+        list1->head = list2->head; // If list1 is empty, point to list2's head
     } else {
         struct _cl_node *current = list1->head;
         while (current->next != NULL) {
-            current = current->next;
+            current = current->next; // Traverse to the end of list1
         }
-        current->next = list2->head;
+        current->next = list2->head; // Join list2 to the end of list1
     }
-    list1->length += list2->length;
-    free(list2); // Free list2 after joining
+
+    list1->length += list2->length; // Update length
 }
 
-// Reverse the list
+// Documented in .h file
 void CL_reverse(CList list) {
     assert(list);
+
     struct _cl_node *prev = NULL;
     struct _cl_node *current = list->head;
     struct _cl_node *next = NULL;
-    
+
     while (current != NULL) {
         next = current->next; // Store next node
         current->next = prev; // Reverse the link
-        prev = current;       // Move prev forward
-        current = next;       // Move current forward
+        prev = current; // Move prev and current forward
+        current = next;
     }
-    list->head = prev; // Reset head to the new front
+    list->head = prev; // Update head to new first node
+}
+
+// Documented in .h file
+void CL_foreach(CList list, CL_foreach_callback callback, void *cb_data) {
+    assert(list);
+    assert(callback);
+
+    struct _cl_node *current = list->head;
+    int pos = 0;
+
+    while (current != NULL) {
+        callback(pos, current->element, cb_data); // Call the user-specified function
+        current = current->next; // Move to the next node
+        pos++;
+    }
 }
